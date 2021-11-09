cda8de4e7a3033c86283f98dcf57294c3884caa6
Igor <ilaptev@ya.ru>
2021-11-09 20:58:28 +0300

    fix: fixed copy-file task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,25 +2,55 @@ const fs = require('fs');
 const path = require('path');
 const dirPath = path.resolve(__dirname, 'files');
 
-fs.mkdir(`${__dirname}/files-copy`, { recursive: true }, (err) => {
-  if (err) throw err;
-});
-
-fs.readdir(dirPath, 
-  { withFileTypes: true },
-  (err, files) => {
-    if (err)
-      console.log(err);
-    else {
-      const copyPath = path.join(__dirname, '/files-copy');
-      files.forEach(file => {
-        if (file.isFile()) {
-          fs.copyFile(`${dirPath}/${file.name}`, `${copyPath}/${file.name}`, (err) => {
-            if (err) {
-              console.log(err);
+fs.stat(`${__dirname}/files-copy`, function(err) {
+  if(err == null) {
+    fs.promises.readdir(`${__dirname}/files-copy`, { withFileTypes: true })
+      .then ((files) => {
+        files.forEach(file => {
+          fs.unlink(`${__dirname}/files-copy/${file.name}`, err => {
+            if (err) throw err;
+          });
+        });
+      });
+    fs.readdir(dirPath, 
+      { withFileTypes: true },
+      (err, files) => {
+        if (err)
+          console.log(err);
+        else {
+          const copyPath = path.join(__dirname, '/files-copy');
+          files.forEach(file => {
+            if (file.isFile()) {
+              fs.copyFile(`${dirPath}/${file.name}`, `${copyPath}/${file.name}`, (err) => {
+                if (err) {
+                  console.log(err);
+                }
+              });
             }
           });
         }
       });
-    }
-  });
+  } else {
+    fs.mkdir(`${__dirname}/files-copy`, { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+    fs.readdir(dirPath, 
+      { withFileTypes: true },
+      (err, files) => {
+        if (err)
+          console.log(err);
+        else {
+          const copyPath = path.join(__dirname, '/files-copy');
+          files.forEach(file => {
+            if (file.isFile()) {
+              fs.copyFile(`${dirPath}/${file.name}`, `${copyPath}/${file.name}`, (err) => {
+                if (err) {
+                  console.log(err);
+                }
+              });
+            }
+          });
+        }
+      });
+  }
+});
